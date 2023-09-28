c209fca39c1dfe01fb0be698dbd04237db09a09d
StefanFox <59245729+StefanFox-Dev@users.noreply.github.com>
2023-09-28 20:49:31 +0300

    Update BedrockStatus

--- BedrockStatus.php
+++ BedrockStatus.php
@@ -1,43 +1,48 @@
 <?php
-$ip = '185.17.0.58'; //ip server
-$port = 19132; // port server
-$status = 'null';
+$ip = '185.17.0.58'; #айпи / ip server
+$port = 19132; #порт / port server
+$status = 'null'; $txt = "🟠 Подключение к серверу $ip:$port";
 
+#Отправка сообщений в Телеграм, ВК / Send message to Telegram, VK
 function send(string $message = ''): void {
- $botTGtoken = 'bot65:aa'; //token bot Telegram
- $botVKtoken = 'vk1.71'; //token bot VK
- $cTGid = '1234'; //chat id Telegram
- $cVKid = 1; //chat id VK
+    $token = 'bot1234:TEST-test'; #токен бота / token bot
+    $id = '-10071'; #айди чата / chat id
 
-    $url = 'https://api.telegram.org/' . $botTGtoken . '/sendMessage?chat_id=' . $cTGid . '&text=' . urlencode($message);
+    $url = 'https://api.telegram.org/' . $token . '/sendMessage?chat_id=' . $id . '&text=' . urlencode($message);
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $result = curl_exec($ch);
     curl_close($ch);
-    
+
     $url = 'https://api.vk.com/method/messages.send';
     $params = [
-        'access_token' => $botVKtoken,
+        'access_token' => 'vk1.a.', #токен / token
         'v' => '5.85'
     ];
     $params['message'] = $message;
-    $params['chat_id'] = $cVKid;
+    $params['chat_id'] = 1; #айди чата / id chat
     $ch = curl_init($url . '?' . http_build_query($params));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $response = curl_exec($ch);
     curl_close($ch);
 }
 
-echo "🟠 Подключение к серверу $ip:$port";
+send($txt);
+echo $txt;
+
+
+#ЗАПУСК / START
 while (true) {
     $socket = fsockopen("udp://" . gethostbyname($ip), $port);
 
     if (!$socket) {
+        #ОШИБКА / ERROR
         if ($status === 'true' || $status === 'null') {
             $status = 'false';
-            send("[" . date('d-m-Y H:i:s') . "/$port] > Статус сервера изменился на 🔴 (socket)");
-            echo PHP_EOL . date('d-m-Y H:i:s') . "/$port 🔴 (socket)";
+            $txt = "[" . date('d-m-Y H:i:s') . "/$port] > Статус сервера изменился на 🔴 (socket)";
+            send($txt);
+            echo PHP_EOL . $txt;
         }
     } else {
         $request_packet = "\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\xff\xff\x00\xfe\xfe\xfe\xfe\xfd\xfd\xfd\xfd\x12\x34\x56\x78\x00\x00\x00\x00\x00\x00\x00\x00";
@@ -47,10 +52,12 @@ while (true) {
         $response = fread($socket, 4096);
 
         if (!$response) {
+            #НЕДОСТУПЕН / NOT AVAILABLE
             if ($status === 'true' || $status === 'null') {
                 $status = 'false';
-                send("[" . date('d-m-Y H:i:s') . "/$port] > Статус сервера изменился на 🔴 (response)");
-                echo PHP_EOL . date('d-m-Y H:i:s') . "/$port 🔴 (response)";
+                $txt = "[" . date('d-m-Y H:i:s') . "/$port] > Статус сервера изменился на 🔴 (response)";
+                send($txt);
+                echo PHP_EOL . $txt;
             }
         } else {
             $server_players = substr($response, 35);
@@ -60,14 +67,17 @@ while (true) {
             if (isset($server_players[4])) {
                 $numplayers = $server_players[4];
             } else $numplayers = 0;
-
+            
+            #УСПЕШНО / SUCCESSFULLY
             if ($status === 'false' || $status === 'null') {
                 $status = 'true';
-                send("[" . date('d-m-Y H:i:s') . "/$port] > Статус сервера изменился на 🟢\nОнлайн: " . $numplayers);
-                echo PHP_EOL . date('d-m-Y H:i:s') . "/$port 🟢\nОнлайн: " . $numplayers;
+                $txt = "[" . date('d-m-Y H:i:s') . "/$port] > Статус сервера изменился на 🟢\nОнлайн: " . $numplayers;
+                send();
+                echo PHP_EOL . $txt;
             }
         }
     }
-    sleep(5);
+    
+    sleep(5); #время проверки / time check
 }
 ?>
